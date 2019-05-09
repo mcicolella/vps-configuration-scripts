04d05be3fa1c813783be66a24c7863990912e65e
Mauro Cicolella <mcicolella@libero.it>
2019-05-09 16:47:09 +0200

    Update configure.sh

--- ubuntu-desktop/configure.sh
+++ ubuntu-desktop/configure.sh
@@ -7,4 +7,10 @@ sudo apt-get upgrade
 # Add npm
 sudo apt-get install npm
 
+# Add Node.js
+# Add PPA for current LTS version
+sudo apt-get install curl python-software-properties
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install nodejs
+
 
