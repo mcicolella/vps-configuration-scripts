6e513b16f844b1632e08a876a55e4de02f779516
Mauro Cicolella <mcicolella@libero.it>
2019-05-09 16:52:57 +0200

    Update configure.sh

--- ubuntu-desktop/configure.sh
+++ ubuntu-desktop/configure.sh
@@ -4,6 +4,12 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+# Add Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
 # Add npm
 sudo apt-get install npm
 
